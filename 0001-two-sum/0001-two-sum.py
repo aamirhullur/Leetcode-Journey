964f18df5803fc625c2335cc968513c2079e8ce3
Aamir Hullur <90979384+Aamir-Hullur@users.noreply.github.com>
2023-08-26 00:34:46 -0400

    Time: 6221 ms (5.00%), Space: 14.1 MB (87.38%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        numsLength = len(nums)
+        for idx1 in range(numsLength):
+            for idx2 in range(numsLength):
+                # You may not use the same element twice.
+                if idx1 == idx2:
+                    continue
+                # Return indexes of two numbers in current iteration that add up to target
+                currSum = nums[idx1] + nums[idx2]
+                if currSum == target:
+                    return [idx1, idx2]
