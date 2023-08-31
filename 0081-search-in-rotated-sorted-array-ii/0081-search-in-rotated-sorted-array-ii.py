20625c460427389ea6cafc9041d64e4ce505e459
Aamir Hullur <90979384+Aamir-Hullur@users.noreply.github.com>
2023-08-30 22:46:20 -0400

    Time: 35 ms (40.29%), Space: 13.6 MB (96.92%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return(True)
+            else:
+                continue
+                
+        return(False)
+            
+        
