67d66ae8973d5e1778d96ef6d8d21407eab773a1
Aamir Hullur <90979384+Aamir-Hullur@users.noreply.github.com>
2023-12-25 18:45:13 -0500

    Time: 795 ms (28.33%), Space: 22.8 MB (8.69%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,10 +4,20 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        minPrice , maxProfit = float('inf'), 0
-        for price in prices:
-            if price < minPrice:
-                minPrice = price
-            elif price - minPrice > maxProfit:
-                maxProfit = price - minPrice
+        # minPrice , maxProfit = float('inf'), 0
+        # for price in prices:
+        #     if price < minPrice:
+        #         minPrice = price
+        #     elif price - minPrice > maxProfit:
+        #         maxProfit = price - minPrice
+        # return maxProfit
+        l,r = 0,1
+        maxProfit = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l] 
+                maxProfit = max(profit,maxProfit)
+            else:
+                l = r
+            r += 1
         return maxProfit
