c1396d7d9071663cc3a2fbe8acc0112713aa337a
Aamir Hullur <90979384+Aamir-Hullur@users.noreply.github.com>
2024-03-06 23:15:45 -0500

    Time: 402 ms (17.19%), Space: 11.8 MB (94.94%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,21 +4,45 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        if len(s) == 1:
-            return 1
-        elif len(s) == 0:
+#         if len(s) == 1:
+#             return 1
+#         elif len(s) == 0:
+#             return 0
+#         l,r = 0,1
+#         res = 0
+#         cnt = 1
+#         while r < len(s):
+#             # print(f"{cnt} {res}")
+#             if s[r] in s[l:r]:
+#                 res = max(cnt,res)
+#                 cnt = 0
+#                 l += 1
+#                 r = l
+#             cnt += 1
+#             r += 1
+#         res = max(cnt,res)
+#         return res
+    
+        if len(s) == 0:
             return 0
+        elif len(s) == 1:
+            return 1
+        
         l,r = 0,1
         res = 0
         cnt = 1
         while r < len(s):
-            # print(f"{cnt} {res}")
-            if s[r] in s[l:r]:
+            if s[r] not in s[l:r]:
+                cnt+=1
+            else:
                 res = max(cnt,res)
-                cnt = 0
+                cnt = 1
                 l += 1
                 r = l
-            cnt += 1
-            r += 1
-        res = max(cnt,res)
+            r+=1
+            # print(f'r: {r},cnt: {cnt}')
+        res = max(cnt,res)        
+    
         return res
+    
+    
