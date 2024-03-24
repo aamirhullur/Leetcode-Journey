419839238318a33c1f0263a26acb6051cbe0c019
Aamir Hullur <90979384+Aamir-Hullur@users.noreply.github.com>
2024-03-24 18:56:08 -0400

    Update sync_script.py

--- sync_script.py
+++ sync_script.py
@@ -42,7 +42,7 @@ def get_last_commit_date(file_path):
     
     # git_command = f"git log -1 --pretty=format:%cd --date=format:'%Y-%m-%d %H:%M:%S' {file_path}"
     print(os.getcwd())
-    git_command = f"git log -1 --pretty=format:%cd --date=format:'%Y-%m-%d %H:%M:%S' '/0206-reverse-linked-list'"
+    git_command = f"git log -1 --pretty=format:%cd --date=format:'%Y-%m-%d %H:%M:%S' '0206-reverse-linked-list'"
     process = subprocess.run(git_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     output = process.stdout.strip()
     print(f'output - {output}')
