c2baaf077f7b222cb9c63f4e087e7687b13fb6b5
Aamir Hullur <90979384+Aamir-Hullur@users.noreply.github.com>
2024-03-24 19:08:55 -0400

    Update sync_script.py

--- sync_script.py
+++ sync_script.py
@@ -43,9 +43,9 @@ def get_last_commit_date(file_path):
     # git_command = f"git log -1 --pretty=format:%cd --date=format:'%Y-%m-%d %H:%M:%S' {file_path}"
     print(os.getcwd())
     
-    commit_date_str = subprocess.check_output(['git', 'log', '-1', '--format=%cd', "--date=format'%Y-%m-%d %H:%M:%S'" , file_path]).decode('utf-8').strip()
-    commit_date = datetime.strptime(commit_date_str, '%a %b %d %H:%M:%S %Y %z').strftime('%Y-%m-%d %H:%M:%S')
-    print(f'commit_date:{commit_date}')
+    # commit_date_str = subprocess.check_output(['git', 'log', '-1', '--format=%cd', "--date=format'%Y-%m-%d %H:%M:%S'" , file_path]).decode('utf-8').strip()
+    # commit_date = datetime.strptime(commit_date_str, '%a %b %d %H:%M:%S %Y %z').strftime('%Y-%m-%d %H:%M:%S')
+    # print(f'commit_date:{commit_date}')
 
     new_path = os.path.join('/home/runner/work/Leetcode-Journey/Leetcode-Journey/', file_path)
     new_command = f"git log -1 --pretty=format:%cd --date=format:'%Y-%m-%d %H:%M:%S' {new_path}"
