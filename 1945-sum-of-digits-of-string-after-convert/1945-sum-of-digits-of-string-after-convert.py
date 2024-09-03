7579dd8e2fd7a7a33bbb1d58894032e341af08c1
Aamir Hullur <90979384+Aamir-Hullur@users.noreply.github.com>
2024-09-03 19:50:51 -0400

    Time: 18 ms (71.43%), Space: 11.8 MB (24.06%) - LeetHub

--- 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.py
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.py
@@ -5,12 +5,19 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        res = ''
-        for i in s:
-            temp = ord(i) - 96
-            res += str(temp)
+#         res = ''
+#         for i in s:
+#             temp = ord(i) - 96
+#             res += str(temp)
 
-        for i in range(k):
-            res = sum(int(x) for x in res)
-            res = str(res)
+#         for i in range(k):
+#             res = sum(int(x) for x in res)
+#             res = str(res)
+#         return int(res)
+
+        res = ''.join([str(ord(i) - 96) for i in s])
+    
+        for _ in range(k):
+            res = str(sum([int(x) for x in res]))
+            
         return int(res)
