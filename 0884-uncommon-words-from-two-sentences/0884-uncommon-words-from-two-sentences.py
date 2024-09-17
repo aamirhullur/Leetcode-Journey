1e9b7fa263a04015dd962f77f04e8d2dc8923c9b
Aamir Hullur <90979384+Aamir-Hullur@users.noreply.github.com>
2024-09-17 12:48:17 -0400

    Time: 16 ms (46.88%), Space: 11.8 MB (34.12%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
@@ -5,31 +5,43 @@ class Solution(object):
         :type s2: str
         :rtype: List[str]
         """
-        string = ''
-        hashmap = {}
-        for i in range(len(s1)):
-            if s1[i] == ' ':
-                hashmap[string] = hashmap.get(string, 0) + 1
-                string = ''
-            else:
-                string = string + s1[i]
+#         string = ''
+#         hashmap = {}
+#         for i in range(len(s1)):
+#             if s1[i] == ' ':
+#                 hashmap[string] = hashmap.get(string, 0) + 1
+#                 string = ''
+#             else:
+#                 string = string + s1[i]
         
-        hashmap[string] = hashmap.get(string, 0) + 1
-        string = ''
+#         hashmap[string] = hashmap.get(string, 0) + 1
+#         string = ''
     
-        for i in range(len(s2)):
-            if s2[i] == ' ':
-                hashmap[string] = hashmap.get(string, 0) + 1
-                string = ''
-            else:
-                string = string + s2[i]
+#         for i in range(len(s2)):
+#             if s2[i] == ' ':
+#                 hashmap[string] = hashmap.get(string, 0) + 1
+#                 string = ''
+#             else:
+#                 string = string + s2[i]
         
-        hashmap[string] = hashmap.get(string, 0) + 1
-        string = ''
+#         hashmap[string] = hashmap.get(string, 0) + 1
+#         string = ''
     
-        result = []
-        for key in hashmap:
-            if hashmap[key] == 1:
-                result.append(key)
+#         result = []
+#         for key in hashmap:
+#             if hashmap[key] == 1:
+#                 result.append(key)
+        
+#         return result
+
+        words = s1.split() + s2.split()
+        
+        # Count the frequency of each word
+        word_count = {}
+        for word in words:
+            word_count[word] = word_count.get(word, 0) + 1
+        
+        # Collect words that appear exactly once
+        result = [word for word in word_count if word_count[word] == 1]
         
         return result
