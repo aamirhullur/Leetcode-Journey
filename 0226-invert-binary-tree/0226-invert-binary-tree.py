0813e1f8a9d2a882ee7061c2a1e8d8a281d4031c
Aamir Hullur <90979384+Aamir-Hullur@users.noreply.github.com>
2025-01-21 10:03:04 -0500

    Time: 0 ms (100%), Space: 12.5 MB (17.43%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -10,17 +10,60 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: Optional[TreeNode]
         """
+        ### Recursive
+
+        # if not root:
+        #     return None
+        
+        # root.left, root.right = root.right, root.left
+
+        # self.invertTree(root.left)
+        # self.invertTree(root.right)
+
+        # return root
+
+
+        ###Iterative DFS
+        # if not root:
+        #     return None
+
+        # stack = [root]
+
+        # while stack:
+        #     node = stack.pop()
+
+        #     node.left, node.right = node.right, node.left
+
+        #     if node.left:
+        #         stack.append(node.left)
+            
+        #     if node.right:
+        #         stack.append(node.right)
+
+        # return root
+
+        
+        ###Iterative BFS
+
         if not root:
             return None
 
-        root.left, root.right = root.right, root.left
-        
-        self.invertTree(root.left)
-        self.invertTree(root.right)
+        queue = deque([root])
+
+        while queue:
+            node = queue.popleft()
+
+            node.left, node.right = node.right, node.left
+
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
 
         return root
 
 
+        
 
 
 
