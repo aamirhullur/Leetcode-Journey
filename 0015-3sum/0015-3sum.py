580f8efab01e600aad7d6bdee35fc76766a97f01
Aamir Hullur <90979384+Aamir-Hullur@users.noreply.github.com>
2025-01-27 15:46:10 -0500

    Time: 1762 ms (27.43%), Space: 20.4 MB (7.84%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -6,24 +6,19 @@ class Solution(object):
         """
         nums.sort()
         res = set()
-        # for i,r in enumerate((nums)):
-        for i in range(0,len(nums)-2):
-            e = nums[i]
-            target = nums[i]*-1
-    
-            l,r = i+1, len(nums)-1
-            while l < r:
-                curSum = nums[l] + nums[r]
-                if curSum > target:
-                    r-=1
-                elif curSum < target:
-                    l+=1
-                elif curSum == target:
-                    res.add((e, nums[l], nums[r])) 
-                    while l < r and nums[l] == nums[l + 1]:
-                        l += 1
-                    while l < r and nums[r] == nums[r - 1]:
-                        r -= 1
-                    l+=1
-                    r-=1
-        return [list(item) for item in res]
+
+        for i in range(len(nums)-2):
+            prev = nums[i]
+            L, R = i+1, len(nums) - 1
+            target = nums[i] * -1
+            while L < R:
+                if nums[L] + nums[R] > target:
+                    R-=1
+                elif nums[L] + nums[R] < target:
+                    L+=1
+                else:
+                    res.add((nums[i],nums[L],nums[R]))
+                    L+=1
+                    R-=1
+
+        return [list(i) for i in res]
