8905b31f14518c536207a1ae61f00bdc9ff3ef17
Aamir Hullur <90979384+Aamir-Hullur@users.noreply.github.com>
2025-01-27 16:07:42 -0500

    Time: 67 ms (93.11%), Space: 20.8 MB (10.35%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -4,21 +4,15 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        res = 0
-        l , r = 0 , len(height) -1 
-        while r - l > 0:
-            # print(l,r)
-            print(height[l],height[r])
-            # area = min(height[i-1],height[j-1]) * abs(i-j)
-            area = min(height[l], height[r]) * (r-l)
-            res = max(res, area)
-            if height[l] < height[r]:
-                l += 1
-            elif height[r] < height[l]:
-                r -= 1
-            elif height[r] == height[l]:
-                if height[l+1] > height[r-1]:
-                    l+=1
-                else:
-                    r-=1
-        return res
+        maxWater = float('-inf')
+        L, R = 0, len(height) - 1
+        W = 0
+        while L < R:
+            if height[L] < height[R]:
+                W = (R-L)*height[L]
+                L +=1
+            else:
+                W = (R-L)*height[R]
+                R -=1
+            maxWater = max(maxWater, W)
+        return maxWater
