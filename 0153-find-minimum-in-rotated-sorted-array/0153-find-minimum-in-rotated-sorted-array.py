8bb3c696525658baaad7f91984ecabbf0e32220a
Aamir Hullur <90979384+Aamir-Hullur@users.noreply.github.com>
2025-02-02 14:49:32 -0500

    Time: 0 ms (100%), Space: 12.5 MB (49.7%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -4,20 +4,35 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        start , end = 0, len(nums) - 1 
-        curr_min = float("inf")
+        # L,R = 0, len(nums)-1
+        # res = nums[0]
+
+        # while L <= R:
+        #     if nums[L] < nums[R]:
+        #         res = min(res, nums[L])
+        #         break
+
+        #     m = (L + R)//2
+        #     res = min(res, nums[m])
+        #     if nums[m] >= L:
+        #         L = m+1
+        #     else:
+        #         R = m-1
         
-        while start  <  end :
-            mid = start + (end - start ) // 2
-            curr_min = min(curr_min,nums[mid])
+        # return res
+
+        res = nums[0]
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            if nums[l] < nums[r]:
+                res = min(res, nums[l])
+                break
             
-            # right has the min 
-            if nums[mid] > nums[end]:
-                start = mid + 1
-                
-            # left has the  min 
+            m = (l + r) // 2
+            res = min(res, nums[m])
+            if nums[m] >= nums[l]:
+                l = m + 1
             else:
-                end = mid - 1 
-                
-        return min(curr_min,nums[start])
-        
+                r = m - 1
+        return res
