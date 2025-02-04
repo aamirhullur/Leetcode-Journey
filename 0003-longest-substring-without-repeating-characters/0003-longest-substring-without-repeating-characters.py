6cce153eb6061e986506a8ec2ac23250cb5683fc
Aamir Hullur <90979384+Aamir-Hullur@users.noreply.github.com>
2025-02-04 17:30:17 -0500

    Time: 58 ms (22.83%), Space: 13.2 MB (32.94%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,45 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-#         if len(s) == 1:
-#             return 1
-#         elif len(s) == 0:
-#             return 0
-#         l,r = 0,1
-#         res = 0
-#         cnt = 1
-#         while r < len(s):
-#             # print(f"{cnt} {res}")
-#             if s[r] in s[l:r]:
-#                 res = max(cnt,res)
-#                 cnt = 0
-#                 l += 1
-#                 r = l
-#             cnt += 1
-#             r += 1
-#         res = max(cnt,res)
-#         return res
-    
-        if len(s) == 0:
-            return 0
-        elif len(s) == 1:
-            return 1
-        
-        l,r = 0,1
+        hmap = {}
+        l = 0
         res = 0
-        cnt = 1
-        while r < len(s):
-            if s[r] not in s[l:r]:
-                cnt+=1
-            else:
-                res = max(cnt,res)
-                cnt = 1
-                l += 1
-                r = l
-            r+=1
-            # print(f'r: {r},cnt: {cnt}')
-        res = max(cnt,res)        
-    
+        for r in range(len(s)):
+            while s[r] in hmap and l < r:
+                # hmap[s[l]] -= 1
+                hmap.pop(s[l])
+                l+=1
+            hmap[s[r]] = 1
+
+            res = max(res, sum(hmap.values()))
         return res
-    
-    
+
+
+
