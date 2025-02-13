857f626cdedf302a20c06924edf8fe2c3c1538ba
Aamir Hullur <90979384+Aamir-Hullur@users.noreply.github.com>
2025-02-12 20:44:11 -0500

    Time: 236 ms (33.54%), Space: 48.4 MB (48.53%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -10,18 +10,34 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        stack = [[root,float('-inf')]]
-        res = 0
-        while stack:
-            node, max_val = stack.pop()
+        # stack = [[root,float('-inf')]]
+        # res = 0
+        # while stack:
+        #     node, max_val = stack.pop()
 
-            if node:
-                print([node.val,max_val,res])
-                # res += 1 if node.val >= max_val else 0
-                if node.val >= max_val:
-                    max_val = node.val
-                    res += 1
-                stack.append([node.left,max_val])
-                stack.append([node.right,max_val])
+        #     if node:
+        #         print([node.val,max_val,res])
+        #         # res += 1 if node.val >= max_val else 0
+        #         if node.val >= max_val:
+        #             max_val = node.val
+        #             res += 1
+        #         stack.append([node.left,max_val])
+        #         stack.append([node.right,max_val])
+        # return res
+
+        # arr = []
+        self.cnt = 0
+        # self.max_num = root.val
+
+        def dfs(node,value):
+            if not node:
+                return
+            
+            if node.val >= value:
+                self.cnt+=1
+            
+            dfs(node.left,max(value,node.val))
+            dfs(node.right,max(value,node.val))
         
-        return res
+        dfs(root,root.val)
+        return self.cnt
