bb6ebed44c798e997088d183db8b4378f13f2b4d
Aamir Hullur <90979384+Aamir-Hullur@users.noreply.github.com>
2025-02-13 19:50:52 -0500

    Time: 1392 ms (31.25%), Space: 23.1 MB (87.5%) - LeetHub

--- 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.py
+++ 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.py
@@ -5,23 +5,37 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        nums.sort(reverse=True)  # Sort in descending order
+        # nums.sort(reverse=True)  
+        # cnt = 0
+
+        # while len(nums) > 1 and nums[-1] < k:
+        #     cnt += 1
+        #     i, j = nums.pop(), nums.pop()
+        #     calc = (i * 2) + j
+
+        #     l, r = 0, len(nums)
+
+        #     while l < r:
+        #         mid = (l + r) // 2
+        #         if mid < len(nums) and nums[mid] > calc:
+        #             l = mid + 1
+        #         else:
+        #             r = mid
+
+        #     nums.insert(l, calc)
+
+        # return -1 if nums[-1] < k else cnt
+
+        def calc(x,y):
+            return (min(x, y) * 2 + max(x, y))
+
+        heapq.heapify(nums)
         cnt = 0
-        
-        while len(nums) > 1 and nums[-1] < k:
-            cnt += 1
-            i, j = nums.pop(), nums.pop()
-            calc = (i * 2) + j
-            
-            l, r = 0, len(nums)
-            # Find correct insertion point to maintain descending order
-            while l < r:
-                mid = (l + r) // 2
-                if mid < len(nums) and nums[mid] > calc:
-                    l = mid + 1
-                else:
-                    r = mid
-                    
-            nums.insert(l, calc)
+        while len(nums) > 1 and nums[0] <k:
+            cnt +=1
+            i = heapq.heappop(nums)
+            j = heapq.heappop(nums)
+
+            heapq.heappush(nums, calc(i,j))
         
         return -1 if nums[-1] < k else cnt
