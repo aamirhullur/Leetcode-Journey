753597e6572894c38f2e374ca85b4834b7430e41
Aamir Hullur <90979384+Aamir-Hullur@users.noreply.github.com>
2025-02-22 17:56:14 -0500

    Time: 43 ms (92.47%), Space: 54 MB (5.12%) - LeetHub

--- 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -2,18 +2,6 @@
  * @param {string} val
  * @return {Object}
  */
-// var expect = function(val) {
-//     return {
-//         toBe: function(val2){
-//             if (val2 === val) return true;
-//             else throw new Error("Not Equal")
-//         },
-//         notToBe: function(val2){
-//             if (val2 !== val) return true;
-//             else throw new Error("Equal")
-//         }
-//     }
-// };
 
 var expect = function(val){
     return {
@@ -25,7 +13,8 @@ var expect = function(val){
         notToBe: function(val2){
             if (val2 !== val){
                 return true;
-            }else throw new Error("Equal")
+            }
+            else throw new Error("Equal")
         }
     }
 };
