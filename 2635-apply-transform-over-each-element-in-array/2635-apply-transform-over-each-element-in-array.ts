408e56ba23a5fb7a70c9b2ff3ad05007cc5167fc
Aamir Hullur <90979384+Aamir-Hullur@users.noreply.github.com>
2025-02-22 19:55:29 -0500

    Time: 43 ms (97.72%), Space: 56.5 MB (5.01%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.ts
@@ -0,0 +1,8 @@
+function map(arr: number[], fn: (n: number, i: number) => number): number[] {
+    // return arr.map(fn)
+    const res = [];
+    for (const i in arr){
+        res.push(fn(arr[i], Number(i)));
+    }
+    return res;
+};
