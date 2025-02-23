64f91f84b6c167160759b920a3f8cc0383221fa6
Aamir Hullur <90979384+Aamir-Hullur@users.noreply.github.com>
2025-02-22 20:46:54 -0500

    Time: 35 ms (99.07%), Space: 53.7 MB (5.33%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -3,12 +3,22 @@
  * @param {Function} fn
  * @return {number[]}
  */
+// var filter = function(arr, fn) {
+//     var filteredArr = [];
+//     for (var i = 0; i < arr.length; i++) {
+//         if (fn(arr[i], i)) {
+//             filteredArr.push(arr[i]);
+//         }
+//     }
+//     return filteredArr;
+// };
+
 var filter = function(arr, fn) {
-    var filteredArr = [];
-    for (var i = 0; i < arr.length; i++) {
-        if (fn(arr[i], i)) {
-            filteredArr.push(arr[i]);
+    let res = [];
+    for (const i in arr){
+        if (fn(arr[i], Number(i))){
+            res.push(arr[i])
         }
     }
-    return filteredArr;
+    return res;
 };
