b220aa1d9b578d2a285dee83a772ab31e93f62aa
Aamir Hullur <90979384+Aamir-Hullur@users.noreply.github.com>
2025-03-13 12:48:19 -0400

    Time: 179 ms (30.8%), Space: 18 MB (48.68%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,22 +1,16 @@
-class Solution(object):
-    def characterReplacement(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
-        count = {}
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        hmap = defaultdict(int)
         res = 0
         l = 0
-
         for r in range(len(s)):
-            count[s[r]] = 1 + count.get(s[r],0)
-
-            while (r-l+1) - max(count.values()) > k:
-                count[s[l]] -= 1
-                l += 1
-
-            res= max(res,(r-l+1))
-
+            hmap[s[r]] += 1
+            while (r-l+1) - max(hmap.values()) > k:
+                hmap[s[l]] -= 1
+                if hmap[s[l]] == 0:
+                    hmap.pop(s[l])
+                l+=1
+            
+            res = max(res,(r-l+1))
 
         return res
