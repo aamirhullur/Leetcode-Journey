7d6e9a7a32c45aaf3c80d0e412d273a268903468
Aamir <90979384+Aamir-Hullur@users.noreply.github.com>
2025-05-01 18:19:11 -0400

    Time: 12 ms (5.94%), Space: 18.1 MB (14.95%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,59 +1,29 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: Optional[ListNode]
-        :type l2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
-        # curr1, curr2 = l1,l2
-
-        # s1,s2 = "",""
-
-        # while curr1 or curr2:
-        #     if curr1:
-        #         s1 = str(curr1.val)+s1
-        #         curr1 = curr1.next
-        #     if curr2:
-        #         s2 = str(curr2.val)+s2
-        #         curr2 = curr2.next
-
-        # s3 = int(s1) + int(s2)
-        # # print(str(s3)[::-1])
-        # node = ListNode(val=None)
-        # curr3= node
-        # for i in str(s3)[::-1]:
-        #     curr3.next = ListNode(int(i))
-        #     curr3 = curr3.next
-        # return node.next
-
-
         dummy = ListNode()
-        cur = dummy
+        res = dummy
 
-        carry = 0
+        total = carry = 0
+
         while l1 or l2 or carry:
-            v1 = l1.val if l1 else 0
-            v2 = l2.val if l2 else 0
+            total = carry
 
-            # new digit
-            val = v1 + v2 + carry
-            carry = val // 10
-            val = val % 10
-            cur.next = ListNode(val)
-
-            # update ptrs
-            cur = cur.next
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-
-        return dummy.next
-
-
-
-
+            if l1:
+                total += l1.val
+                l1 = l1.next
+            if l2:
+                total += l2.val
+                l2 = l2.next
+            
+            num = total % 10
+            carry = total // 10
+            dummy.next = ListNode(num)
+            dummy = dummy.next
+        
+        return res.next
