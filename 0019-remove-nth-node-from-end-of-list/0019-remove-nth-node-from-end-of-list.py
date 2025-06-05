ed2ed863d865a3c43ea253318005f6c0a78fbba8
Aamir <90979384+Aamir-Hullur@users.noreply.github.com>
2025-06-05 15:26:26 -0400

    Time: 0 ms (100%), Space: 17.8 MB (54.74%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -1,67 +1,24 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def removeNthFromEnd(self, head, n):
-        """
-        :type head: Optional[ListNode]
-        :type n: int
-        :rtype: Optional[ListNode]
-        """
-
-
-        # arr = []
-        # curr = head
-
-        # while curr:
-        #     arr.append(curr)
-        #     curr = curr.next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         
-        # if len(arr) == 1:
-        #     return arr[0].next
-        # k = len(arr) - n - 1
-        # if k < 0:
-        #     return head.next
-        # arr[k].next = arr[k].next.next
+        dummy = ListNode(0,head)
 
-        # return head
+        slow = dummy
+        fast = head
 
-        # k = 0 
-        # curr = head
-        # while curr:
-        #     k+=1
-        #     curr = curr.next
-
-        # tar = k -n -1
-        # if tar < 0:
-        #     return head.next
-        # curr = head
-        # while tar > 0:
-        #     curr = curr.next
-        #     tar -=1
-
-        # curr.next = curr.next.next
-
-        # return head
-
-        # if not head.next:
-        #     return head.next
-
-        curr1, curr2 = head,head
-        while curr2 and n>0:
-            curr2 = curr2.next
+        while n>0 and fast:
+            fast = fast.next
             n-=1
-
-        while curr2 and curr2.next:
-            curr2 = curr2.next
-            curr1 = curr1.next
         
-        if curr2:
-            curr1.next = curr1.next.next
-        else:
-            return head.next
+        while fast :
+            fast = fast.next
+            slow = slow.next
+        
+        slow.next = slow.next.next
 
-        return head
-
+        return dummy.next
