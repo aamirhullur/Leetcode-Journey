796570e04e48d1306227ab554999549d37654088
Aamir <90979384+Aamir-Hullur@users.noreply.github.com>
2025-06-09 10:56:29 -0400

    Time: 0 ms (100%), Space: 18.7 MB (17.04%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -1,26 +1,22 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def levelOrder(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         
-        if not root:
-            return []
-        
-        queue = deque([[root,0]])
+        if not root: return []
+
+        queue = deque([[root,0]])   
         res = defaultdict(list)
         while queue:
-            node,dep = queue.popleft()
-            if node:
-                res[dep].append(node.val)
-                queue.append([node.left,dep+1])
-                queue.append([node.right,dep+1])
+            curr, lvl = queue.popleft()
+            if curr:
+                res[lvl].append(curr.val)
 
-        return res.values()
+                queue.append([curr.left,lvl+1])
+                queue.append([curr.right,lvl+1])
+        
+        return(list(res.values()))
