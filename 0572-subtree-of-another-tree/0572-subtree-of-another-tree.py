71513b690ac3627c1b381c52225636b76f2d75af
Aamir <90979384+Aamir-Hullur@users.noreply.github.com>
2025-06-26 17:27:10 -0400

    Time: 46 ms (39.29%), Space: 18 MB (83.14%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -1,57 +1,28 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def isSubtree(self, root, subRoot):
-        """
-        :type root: Optional[TreeNode]
-        :type subRoot: Optional[TreeNode]
-        :rtype: bool
-        """
-
-        # def inorder(node,arr):
-        #     if not node:
-        #         return
-            
-        #     inorder(node.left,arr)
-        #     arr.append(node.val)
-        #     inorder(node.right,arr)
-
-        #     return arr
-
-        def isSameTree(root, subRoot):
-            if not root and not subRoot:
-                return True
-            elif not root or not subRoot:
-                return False
-            elif root.val != subRoot.val:
-                return False
-            
-            return isSameTree(root.left,subRoot.left) and isSameTree(root.right,subRoot.right)
-
-        stack = [[root, subRoot]]
-        
-        while stack:
-
-            root, subRoot = stack.pop()
-            if root and subRoot:
-                if root.val == subRoot.val:
-                    print([root.val,subRoot.val])
-                    # arr1, arr2 = [], []
-                    # t1 = inorder(root,arr1)
-                    # t2 = inorder(subRoot,arr2)
-                    # print([t1,t2])
-                    # if t1 == t2:
-                    #     return True
-                    res = isSameTree(root,subRoot)
-                    if res:
-                        return True
-                stack.append([root.left, subRoot])
-                stack.append([root.right, subRoot])
-            else:
-                continue
-        
+class Solution:
+    def isSame(self,p,q):
+        if not p and not q:
+            return True
+        if p and q and p.val == q.val:
+            return self.isSame(p.left,q.left) and self.isSame(p.right,q.right)
         return False
+
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not root:
+            return False
+        if not subRoot:
+            return True
+
+        if self.isSame(root, subRoot):
+            return True
+        return (self.isSubtree(root.left, subRoot) or 
+               self.isSubtree(root.right, subRoot))
+        
+
+
+        
