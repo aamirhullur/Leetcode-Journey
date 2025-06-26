28a59a46ee20ae64944a9cff7d6e6f3307eb667e
Aamir <90979384+Aamir-Hullur@users.noreply.github.com>
2025-06-26 19:14:05 -0400

    Time: 128 ms (91.16%), Space: 31.7 MB (98.5%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -1,43 +1,25 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def goodNodes(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        # stack = [[root,float('-inf')]]
-        # res = 0
-        # while stack:
-        #     node, max_val = stack.pop()
-
-        #     if node:
-        #         print([node.val,max_val,res])
-        #         # res += 1 if node.val >= max_val else 0
-        #         if node.val >= max_val:
-        #             max_val = node.val
-        #             res += 1
-        #         stack.append([node.left,max_val])
-        #         stack.append([node.right,max_val])
-        # return res
-
-        # arr = []
-        self.cnt = 0
-        # self.max_num = root.val
-
-        def dfs(node,value):
-            if not node:
-                return
-            
-            if node.val >= value:
-                self.cnt+=1
-            
-            dfs(node.left,max(value,node.val))
-            dfs(node.right,max(value,node.val))
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
         
-        dfs(root,root.val)
-        return self.cnt
+        stack = [[root,root.val]]
+        res = 0
+
+        while stack:
+            node, currMax = stack.pop()
+
+            if node.val >= currMax:
+                res+=1
+                currMax = max(currMax,node.val)
+            if node.left:
+                stack.append([node.left,currMax])
+            if node.right:
+                stack.append([node.right,currMax])
+        
+        return res
+                
