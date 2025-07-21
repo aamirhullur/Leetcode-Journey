98cb58b7c69bd6f6f64b555b2c69dbe79a65109d
Aamir <90979384+Aamir-Hullur@users.noreply.github.com>
2025-07-21 18:24:16 -0400

    Time: 27 ms (41.62%), Space: 18.1 MB (77.85%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -6,24 +6,44 @@ class Solution:
 
         res = 0
 
-        def dfs(r,c):
+        # def dfs(r,c):
 
-            if min(r,c) < 0 or r==ROWS or c == COLS or grid[r][c] == 0:
-                return 0
+        #     if min(r,c) < 0 or r==ROWS or c == COLS or grid[r][c] == 0:
+        #         return 0
 
-            grid[r][c]=0
+        #     grid[r][c]=0
+        #     cnt = 1
+            
+        #     directions = [[1,0],[-1,0],[0,1],[0,-1]]
+
+        #     for dx,dy in directions:
+        #         cnt += dfs(r+dx,c+dy)
+            
+        #     return cnt
+
+        visited = set()
+        def bfs(r,c):
+            q = collections.deque()
+            visited.add((r,c))
+            q.append((r,c))
             cnt = 1
-            
-            directions = [[1,0],[-1,0],[0,1],[0,-1]]
-
-            for dx,dy in directions:
-                cnt += dfs(r+dx,c+dy)
-            
+            while q:
+                row, col = q.popleft()
+                directions = [[1,0],[-1,0],[0,1],[0,-1]]
+                for dx,dy in directions:
+                    r = row+dx
+                    c = col+dy
+                    if min(r,c) >= 0 and r < ROWS and c < COLS and grid[r][c] == 1 and (r,c) not in visited:
+                        q.append((r,c))
+                        visited.add((r,c))
+                        cnt+=1
+                        
             return cnt
 
         for i in range(ROWS):
             for j in range(COLS):
-                if grid[i][j] == 1:
-                    res = max(res,dfs(i,j))
+                if grid[i][j] == 1 and (i,j) not in visited:
+                    # res = max(res,dfs(i,j))
+                    res = max(res,bfs(i,j))
         
         return res
