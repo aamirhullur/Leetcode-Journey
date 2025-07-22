8d1cae73220d374e669bb119ada1318a0d6d9ece
Aamir <90979384+Aamir-Hullur@users.noreply.github.com>
2025-07-22 00:37:41 -0400

    Time: 2 ms (86.52%), Space: 17.8 MB (44.57%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -5,7 +5,8 @@ class Solution:
         COLS = len(grid[0])
 
         visited = set()
-
+        rotten_cnt = 0
+        oranges = 0
         q = collections.deque()
         res = 0
         for i in range(ROWS):
@@ -13,12 +14,16 @@ class Solution:
                 if grid[i][j] == 2:
                     q.append((i,j,0))
                     visited.add((i,j))
+                    oranges += 1
+                if grid[i][j] == 1:
+                    oranges += 1
         
         while q:
             r,c,t = q.popleft()
             directions = [[1,0],[-1,0],[0,1],[0,-1]]
             res = max(t,res)
             grid[r][c] = 2
+            rotten_cnt += 1
             for dx,dy in directions:
                 nr = r+dx
                 nc = c+dy
@@ -26,9 +31,12 @@ class Solution:
                     q.append((nr,nc,t+1))
                     visited.add((nr,nc))
 
-        for i in range(ROWS):
-            for j in range(COLS):
-                if grid[i][j] == 1:
-                    return -1
+        # for i in range(ROWS):
+        #     for j in range(COLS):
+        #         if grid[i][j] == 1:
+        #             return -1
 
-        return res
+        if rotten_cnt == oranges:
+            return res
+        else:
+            return -1
