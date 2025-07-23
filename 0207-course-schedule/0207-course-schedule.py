7dc02156a7b812efe47826f439e3de815252e90b
Aamir <90979384+Aamir-Hullur@users.noreply.github.com>
2025-07-23 00:24:28 -0400

    Time: 0 ms (100%), Space: 19.6 MB (16.34%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,28 +1,28 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
 
-        hmap = {i: [] for i in range(numCourses)}
-        for crs,pre in prerequisites:
+        hmap = {i:[] for i in range(numCourses)}
+        for crs, pre in prerequisites:
             hmap[crs].append(pre)
-    
-        visitSet = set()
-
+        
+        visit = set()
         def dfs(crs):
-            if crs in visitSet:
+            if crs in visit:
                 return False
-
+            
             if hmap[crs] == []:
                 return True
 
-            visitSet.add(crs)
-
+            visit.add(crs)
             for pre in hmap[crs]:
-                if not dfs(pre): return False
-            
-            visitSet.remove(crs)
+                if not dfs(pre):
+                    return False
+            visit.remove(crs)
             hmap[crs] = []
             return True
 
-        for crs in range(numCourses):
-            if not dfs(crs): return False
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
         return True
+
