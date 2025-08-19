026bb65b665def6a8b39342f76f7fd3ada43ef4e
Aamir <90979384+aamirhullur@users.noreply.github.com>
2025-08-19 10:35:42 -0400

    Time: 28 ms (23.33%), Space: 19.4 MB (18.16%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,16 +1,34 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        l,r = 0, len(height)-1
+        # l,r = 0,len(height)-1
 
-        res =0 
-        leftmax, rightmax = height[l], height[r]
-        while l<r:
-            if height[l] <= height[r]:
-                l+=1
-                leftmax = max(leftmax, height[l])
-                res += leftmax - height[l]
-            else:
-                r-=1
-                rightmax = max(rightmax, height[r])
-                res += rightmax - height[r]
+        # maxL, maxR = height[l],height[r]
+        # res = 0
+
+        # while l<r:
+        #     if maxL < maxR:
+        #         l+=1
+        #         maxL = max(height[l],maxL)
+        #         res += maxL - height[l]
+        #     else:
+        #         r-=1
+        #         maxR = max(height[r],maxR)
+        #         res += maxR - height[r]
+        # return res
+        N = len(height)
+        maxL = [0]*N
+        maxR = [0]*N
+
+        maxL[0] = height[0]
+        maxR[N-1] = height[N-1]
+
+        for i in range(1,N):
+            maxL[i] = max(height[i],maxL[i-1])
+
+        for i in range(N-2,-1,-1):
+            maxR[i] = max(height[i],maxR[i+1])
+
+        res = 0
+        for i in range(N):
+            res += min(maxL[i], maxR[i]) - height[i]
         return res
