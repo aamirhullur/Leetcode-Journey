e7a2baa58a22557ebf09f0459288337cae13a4d6
Aamir <90979384+aamirhullur@users.noreply.github.com>
2025-08-19 10:38:45 -0400

    Time: 19 ms (40.18%), Space: 19.4 MB (18.16%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,34 +1,36 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # l,r = 0,len(height)-1
+        l,r = 0,len(height)-1
 
-        # maxL, maxR = height[l],height[r]
-        # res = 0
-
-        # while l<r:
-        #     if maxL < maxR:
-        #         l+=1
-        #         maxL = max(height[l],maxL)
-        #         res += maxL - height[l]
-        #     else:
-        #         r-=1
-        #         maxR = max(height[r],maxR)
-        #         res += maxR - height[r]
-        # return res
-        N = len(height)
-        maxL = [0]*N
-        maxR = [0]*N
-
-        maxL[0] = height[0]
-        maxR[N-1] = height[N-1]
-
-        for i in range(1,N):
-            maxL[i] = max(height[i],maxL[i-1])
-
-        for i in range(N-2,-1,-1):
-            maxR[i] = max(height[i],maxR[i+1])
-
+        maxL, maxR = height[l],height[r]
         res = 0
-        for i in range(N):
-            res += min(maxL[i], maxR[i]) - height[i]
+
+        while l<r:
+            if maxL < maxR:
+                l+=1
+                res += max((maxL - height[l]),0)
+                maxL = max(height[l],maxL)
+            else:
+                r-=1
+                res += max((maxR - height[r]),0)
+                maxR = max(height[r],maxR)
         return res
+    
+    
+        # N = len(height)
+        # maxL = [0]*N
+        # maxR = [0]*N
+
+        # maxL[0] = height[0]
+        # maxR[N-1] = height[N-1]
+
+        # for i in range(1,N):
+        #     maxL[i] = max(height[i],maxL[i-1])
+
+        # for i in range(N-2,-1,-1):
+        #     maxR[i] = max(height[i],maxR[i+1])
+
+        # res = 0
+        # for i in range(N):
+        #     res += min(maxL[i], maxR[i]) - height[i]
+        # return res
