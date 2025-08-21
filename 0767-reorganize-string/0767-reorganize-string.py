ca718b48eea9e2ff35f71dab2bae58c5cdfd9d66
Aamir <90979384+aamirhullur@users.noreply.github.com>
2025-08-21 00:17:39 -0400

    Time: 27 ms (5.16%), Space: 17.8 MB (75.06%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -0,0 +1,38 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        # aaabc - > abaca
+        # aabbc -> ababc
+        # aaaabc -> abacaa
+
+        # if most freq element > len(s) - rem -> return ""
+        # max_heap -> pop -> append -> push
+
+        strCnt = collections.Counter(s) # O(n)
+
+        maxHeap = [(-v,k) for k,v in strCnt.items()] # O(n)
+        heapq.heapify(maxHeap) # log(n)
+
+        freq = -maxHeap[0][0]
+        rem = len(s) - freq
+
+        if freq > rem+1:
+            return ""
+
+        res = ""    
+
+        q = collections.deque()
+        time = 1
+        while maxHeap or q:
+            print(time,maxHeap)
+            while q and q[0][0] == time:
+                t,c,val = q.popleft()
+                heapq.heappush(maxHeap,(c,val))
+
+            cnt, s = heapq.heappop(maxHeap) # log(n)
+            res += s
+            cnt += 1
+            if cnt:
+                q.append((time+2,cnt,s))
+            # heapq.heappush(maxHeap, (cnt,s)) # log(n)
+            time += 1
+        return res
