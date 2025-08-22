1e66bccbb9d1d0e8f4ca8cba8b7ce97492aa1af9
Aamir <90979384+aamirhullur@users.noreply.github.com>
2025-08-21 22:32:02 -0400

    Time: 83 ms (48.33%), Space: 18.3 MB (54.75%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,30 +1,95 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if t == "":
+        # if not t or not s:
+        #     return ""
+
+        # # Dictionary which keeps a count of all the unique characters in t.
+        # dict_t = Counter(t)
+
+        # # Number of unique characters in t, which need to be present in the desired window.
+        # required = len(dict_t)
+
+        # # left and right pointer
+        # l, r = 0, 0
+
+        # # formed is used to keep track of how many unique characters in t are present in the current window in its desired frequency.
+        # # e.g. if t is "AABC" then the window must have two A's, one B and one C. Thus formed would be = 3 when all these conditions are met.
+        # formed = 0
+
+        # # Dictionary which keeps a count of all the unique characters in the current window.
+        # window_counts = {}
+
+        # # ans tuple of the form (window length, left, right)
+        # ans = float("inf"), None, None
+
+        # while r < len(s):
+
+        #     # Add one character from the right to the window
+        #     character = s[r]
+        #     window_counts[character] = window_counts.get(character, 0) + 1
+
+        #     # If the frequency of the current character added equals to the desired count in t then increment the formed count by 1.
+        #     if (
+        #         character in dict_t
+        #         and window_counts[character] == dict_t[character]
+        #     ):
+        #         formed += 1
+
+        #     # Try and contract the window till the point where it ceases to be 'desirable'.
+        #     while l <= r and formed == required:
+        #         character = s[l]
+
+        #         # Save the smallest window until now.
+        #         if r - l + 1 < ans[0]:
+        #             ans = (r - l + 1, l, r)
+
+        #         # The character at the position pointed by the `left` pointer is no longer a part of the window.
+        #         window_counts[character] -= 1
+        #         if (
+        #             character in dict_t
+        #             and window_counts[character] < dict_t[character]
+        #         ):
+        #             formed -= 1
+
+        #         # Move the left pointer ahead, this would help to look for a new window.
+        #         l += 1
+
+        #     # Keep expanding the window once we are done contracting.
+        #     r += 1
+        # return "" if ans[0] == float("inf") else s[ans[1] : ans[2] + 1]
+
+
+
+
+        if s == "" or t == "":
             return ""
 
-        countT, window = {}, {}
-        for c in t:
-            countT[c] = 1 + countT.get(c, 0)
+        hmapT = Counter(t)
+        hmapS = {}
 
-        have, need = 0, len(countT)
-        res, resLen = [-1, -1], float("infinity")
-        l = 0
-        for r in range(len(s)):
-            c = s[r]
-            window[c] = 1 + window.get(c, 0)
+        need = len(hmapT)
+        ans = (float('inf'),-1,-1)
+        l,r = 0,0
 
-            if c in countT and window[c] == countT[c]:
+        have = 0
+
+        while r < len(s):
+            char = s[r]
+            hmapS[char] = hmapS.get(char,0) + 1
+
+            if char in hmapT and hmapT[char] == hmapS[char]:
                 have += 1
+            
+            while l<len(s) and have == need:
+                c = s[l]
 
-            while have == need:
-                if (r - l + 1) < resLen:
-                    res = [l, r]
-                    resLen = r - l + 1
-                    
-                window[s[l]] -= 1
-                if s[l] in countT and window[s[l]] < countT[s[l]]:
-                    have -= 1
-                l += 1
-        l, r = res
-        return s[l : r + 1] if resLen != float("infinity") else ""
+                if (r-l+1) < ans[0]:
+                    ans = (r-l+1,l,r)
+
+                hmapS[c] -= 1
+                if c in hmapT and hmapS[c] < hmapT[c]:
+                    have-=1
+                
+                l+=1
+            r+=1
+        return s[ans[1]:ans[2]+1]
